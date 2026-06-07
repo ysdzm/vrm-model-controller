5a4129d56048a1ff020d6da00a21d13c67667d90
dokudami <69578376+ysdzm@users.noreply.github.com>
2026-06-07 22:32:31 +0900

    Add look at controls

--- Packages/com.ysdzm.vrm-model-controller/Runtime/VrmModelController.cs
+++ Packages/com.ysdzm.vrm-model-controller/Runtime/VrmModelController.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 
 namespace Ysdzm.VrmModelController
 {
+    public enum LookAtMode
+    {
+        None,
+        EyesOnly,
+        Head
+    }
+
+    [DefaultExecutionOrder(11020)]
     public class VrmModelController : MonoBehaviour
     {
         private static readonly ExpressionPreset[] ResetExpressionPresets =
@@ -26,42 +34,54 @@ namespace Ysdzm.VrmModelController
             ExpressionPreset.lookRight
         };
 
-        [SerializeField] private Transform targetRoot;
         [SerializeField] private Vrm10Instance vrmInstance;
+        [SerializeField] private Transform lookAtTarget;
+        [SerializeField] private LookAtMode lookAtMode = LookAtMode.None;
+        [SerializeField] private float headLookAtSpeed = 12f;
 
-        public Transform TargetRoot => targetRoot != null ? targetRoot : transform;
+        private Transform head;
+        private Quaternion initialHeadLocalRotation;
+        private bool hasInitialHeadLocalRotation;
+
         public Vrm10Instance VrmInstance => ResolveVrmInstance();
         public bool HasVrmInstance => VrmInstance != null;
         public string VrmInstanceName => VrmInstance != null ? VrmInstance.name : string.Empty;
+        public LookAtMode CurrentLookAtMode => lookAtMode;
+        public Transform LookAtTarget => lookAtTarget;
+
+        private void LateUpdate()
+        {
+            UpdateLookAt();
+        }
 
         public Vector3 GetLocalPosition()
         {
-            return TargetRoot.localPosition;
+            return transform.localPosition;
         }
 
         public Vector3 GetLocalRotation()
         {
-            return TargetRoot.localEulerAngles;
+            return transform.localEulerAngles;
         }
 
         public float GetUniformScale()
         {
-            return TargetRoot.localScale.x;
+            return transform.localScale.x;
         }
 
         public void SetLocalPosition(Vector3 position)
         {
-            TargetRoot.localPosition = position;
+            transform.localPosition = position;
         }
 
         public void SetLocalRotation(Vector3 eulerAngles)
         {
-            TargetRoot.localEulerAngles = eulerAngles;
+            transform.localEulerAngles = eulerAngles;
         }
 
         public void SetUniformScale(float scale)
         {
-            TargetRoot.localScale = Vector3.one * scale;
+            transform.localScale = Vector3.one * scale;
         }
 
         public void ResetTransform()
@@ -91,24 +111,125 @@ namespace Ysdzm.VrmModelController
 
         public void ProcessVrmRuntime()
         {
-            var instance = VrmInstance;
-            if (instance == null || instance.Runtime == null)
+            if (VrmInstance == null)
             {
                 return;
             }
 
-            instance.Runtime.Process();
+            VrmInstance.Runtime.Process();
+        }
+
+        public void SetLookAtTarget(Transform target)
+        {
+            lookAtTarget = target;
+            ApplyEyeLookAtSettings();
+        }
+
+        public void SetLookAtMode(LookAtMode mode)
+        {
+            lookAtMode = mode;
+            ApplyEyeLookAtSettings();
+        }
+
+        public void ClearLookAtTarget()
+        {
+            SetLookAtTarget(null);
         }
 
         private void SetExpressionWeight(ExpressionKey key, float weight)
         {
-            var instance = VrmInstance;
-            if (instance == null || instance.Runtime == null)
+            if (VrmInstance == null)
             {
                 return;
             }
 
-            instance.Runtime.Expression.SetWeight(key, Mathf.Clamp01(weight));
+            VrmInstance.Runtime.Expression.SetWeight(key, Mathf.Clamp01(weight));
+        }
+
+        private void UpdateLookAt()
+        {
+            ApplyEyeLookAtSettings();
+
+            if (lookAtMode == LookAtMode.EyesOnly)
+            {
+                ResetHeadRotation();
+                return;
+            }
+
+            if (lookAtMode == LookAtMode.Head)
+            {
+                ApplyHeadLookAt();
+            }
+        }
+
+        private void ApplyEyeLookAtSettings()
+        {
+            if (VrmInstance == null)
+            {
+                return;
+            }
+
+            if (lookAtMode == LookAtMode.EyesOnly && lookAtTarget != null)
+            {
+                VrmInstance.LookAtTarget = lookAtTarget;
+                VrmInstance.LookAtTargetType = VRM10ObjectLookAt.LookAtTargetTypes.SpecifiedTransform;
+                return;
+            }
+
+            VrmInstance.LookAtTarget = null;
+            VrmInstance.LookAtTargetType = VRM10ObjectLookAt.LookAtTargetTypes.YawPitchValue;
+        }
+
+        private void ApplyHeadLookAt()
+        {
+            if (lookAtTarget == null || !TryGetHead(out var headBone))
+            {
+                return;
+            }
+
+            var direction = lookAtTarget.position - headBone.position;
+            if (direction.sqrMagnitude <= Mathf.Epsilon)
+            {
+                return;
+            }
+
+            var targetRotation = Quaternion.LookRotation(direction.normalized, transform.up);
+            headBone.rotation = Quaternion.Slerp(headBone.rotation, targetRotation, Time.deltaTime * headLookAtSpeed);
+        }
+
+        private void ResetHeadRotation()
+        {
+            if (!TryGetHead(out var headBone))
+            {
+                return;
+            }
+
+            headBone.localRotation = initialHeadLocalRotation;
+        }
+
+        private bool TryGetHead(out Transform headBone)
+        {
+            if (head != null)
+            {
+                headBone = head;
+                return true;
+            }
+
+            var instance = VrmInstance;
+            if (instance == null || !instance.TryGetBoneTransform(HumanBodyBones.Head, out head))
+            {
+                headBone = null;
+                return false;
+            }
+
+            if (!hasInitialHeadLocalRotation)
+            {
+                initialHeadLocalRotation = head.localRotation;
+                hasInitialHeadLocalRotation = true;
+            }
+
+            headBone = head;
+            return true;
         }
 
         private Vrm10Instance ResolveVrmInstance()
@@ -119,8 +240,8 @@ namespace Ysdzm.VrmModelController
             }
 
             return vrmInstance =
-                TargetRoot.GetComponentInChildren<Vrm10Instance>()
-                ?? TargetRoot.GetComponentInParent<Vrm10Instance>()
+                GetComponentInChildren<Vrm10Instance>()
+                ?? GetComponentInParent<Vrm10Instance>()
                 ?? FindFirstObjectByType<Vrm10Instance>();
         }
     }
